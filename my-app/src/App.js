5df08a4e269b50bfb702a4faee62327a1dd6322f
N.Selman Duman <selman@192.168.1.56>
2021-10-19 14:35:31 +0300

    First react app created

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <h1>My first react app!</h1>
+    </div>
+  );
+}
+
+export default App;
